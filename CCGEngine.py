1301f442bb0f3d781e7ac7108ce1730fea93a43c
Luke Sjulson <lukesjulson@gmail.com>
2023-07-02 10:03:33 -0400

    fixed bug in wrapper function (which is still untested) and updated the comments.

--- CCGEngine.py
+++ CCGEngine.py
@@ -23,9 +23,18 @@ lib.CCGEngine.argtypes = [ctypes.c_int,
                           ctypes.POINTER(ctypes.c_uint)]
 
 
-def CCGEngine(times, groups, binSize_Fs, halfBins):
+def CCGEngine(times, marks, binSize_Fs, halfBins):
+	# times is a double array of spike times
+	# marks is a uint array of numbers labeling each neuron, e.g. 1, 2, 3 for neurons 1-3.
+	# It has to start at 1, not 0. This wrapper function should take a more versatile type
+	# of label and convert it to the "marks" format used by the C function.
+	# binSize_Fs is the binsize in the same units as times
+	# halfBins is the number of bins you want to calculate. The total number of bins calculated
+	# will be (2*halfBins + 1)
+
+
     # Initialize output arrays
-    CountArraySize = np.max(groups)**2 * (2*halfBins + 1)
+    CountArraySize = np.max(marks)**2 * (2*halfBins + 1)
 
     # Initialize output arrays
     Count = np.zeros((CountArraySize,), dtype=np.uint32)
@@ -34,21 +43,21 @@ def CCGEngine(times, groups, binSize_Fs, halfBins):
 
 	# Convert numpy arrays to ctypes arrays
 	Times_ctypes = Times.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-	Marks_ctypes = Marks.ctypes.data_as(ctypes.POINTER(ctypes.c_uint))
+	marks_ctypes = marks.ctypes.data_as(ctypes.POINTER(ctypes.c_uint))
 	Count_ctypes = Count.ctypes.data_as(ctypes.POINTER(ctypes.c_uint))
 	Pairs_ctypes = Pairs.ctypes.data_as(ctypes.POINTER(ctypes.c_uint))
 
 	# call the C function
-	lib.CCGEngine(len(times), Times_ctypes, Marks_ctypes, BinSize, HalfBins, Count_ctypes, Pairs_ctypes)
+	lib.CCGEngine(len(times), Times_ctypes, marks_ctypes, BinSize, HalfBins, Count_ctypes, Pairs_ctypes)
 
     # Reshape Count to (nBins, n, n)
-    n = np.max(groups)
+    n = np.max(marks)
     nBins = 2*halfBins+1
     Count = np.reshape(Count, [nBins, n, n])
     
     # Pad Count if necessary
-    if n < np.max(groups):
-        Count = np.pad(Count, ((0, 0), (0, np.max(groups) - n), (0, np.max(groups) - n)), mode='constant')
+    if n < np.max(marks):
+        Count = np.pad(Count, ((0, 0), (0, np.max(marks) - n), (0, np.max(marks) - n)), mode='constant')
 
     return Count
 
